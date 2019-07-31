f6b385a27e6fd8d85a4bf46da642ecac11226a58
Jeongmin Cho <jeongcho96@gmail.com>
2019-07-31 15:28:06 -0400

    added route to POST new meal order

--- server/api/routes/meals.js
+++ server/api/routes/meals.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const Meal = require('../../db/models/meal')
+const {Meal, MealOrder, Order} = require('../../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
@@ -15,6 +15,28 @@ router.get('/:id', async (req, res, next) => {
   try {
     const singleMeal = await Meal.findByPk(req.params.id)
     res.json(singleMeal)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// ADD TO CART ROUTE
+router.post('/:id', async (req, res, next) => {
+  try {
+    const userCart = await Order.findOne({
+      where: {
+        userId: req.user.id,
+        isCart: true
+      }
+    })
+    const newMealOrder = {
+      quantity: 1,
+      mealId: req.params.id,
+      orderId: userCart.id
+    }
+    const addedMealOrder = await MealOrder.create(newMealOrder)
+    // UPDATE TOTAL PRICE OF ORDER
+    res.json(addedMealOrder)
   } catch (err) {
     next(err)
   }
