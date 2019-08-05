1e4034c82aa819abb8b8ed05f6b53523bdf25f90
Shan Hu <nahsuh@hotmail.com>
2019-08-05 12:39:03 -0400

    backend datavalidation

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,30 +1,14 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
-const MealOrder = require('./mealOrder')
 
 const Order = db.define('order', {
   isCart: {
     type: Sequelize.BOOLEAN,
-    allowNull: false
+    defaultValue: true
   },
   totalPrice: {
     type: Sequelize.INTEGER
   }
 })
-
-Order.prototype.setTotalPrice = async function() {
-  // find all meals associated with this order
-  const mealOrders = await MealOrder.findAll({
-    where: {
-      orderId: this.id
-    }
-  })
-  let totalPrice = 0
-  mealOrders.forEach(mealOrder => {
-    totalPrice += mealOrder.price
-  })
-  this.totalPrice = totalPrice
-  this.save()
-}
 
 module.exports = Order
